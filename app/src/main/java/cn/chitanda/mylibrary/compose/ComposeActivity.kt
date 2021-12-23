c9f2047ac6e4f63e6d3f8d72afb01c521375d5ea
chunjinchen <chunjinchen1998@gmail.com>
2021-12-23 10:09:16 +0800

    modify: 更新模块版本号

--- app/src/main/java/cn/chitanda/mylibrary/compose/ComposeActivity.kt
+++ app/src/main/java/cn/chitanda/mylibrary/compose/ComposeActivity.kt
@@ -1,12 +1,8 @@
 package cn.chitanda.mylibrary.compose
 
-import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,22 +15,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.NativeCanvas
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import cn.chitanda.dynamicstatusbar.DynamicStatusBar
-import cn.chitanda.dynamicstatusbar.NativeAnalyst
 import cn.chitanda.mylibrary.compose.ui.theme.MyLibraryTheme
-import kotlin.math.roundToInt
 
 private const val TAG = "ComposeActivity"
+
 class ComposeActivity : ComponentActivity() {
-    private  val nativeAnalyst = NativeAnalyst()
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-//        DynamicStatusBar.init(this)
         setContent {
             MyLibraryTheme {
                 // A surface container using the 'background' color from the theme
@@ -56,18 +47,5 @@ class ComposeActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyLibraryTheme {
-        Greeting("Android")
     }
 }
