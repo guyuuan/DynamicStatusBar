98600e39816882ee0b75550dcfd386a5633f8580
chunjinchen <chunjinchen1998@gmail.com>
2022-01-05 10:49:40 +0800

    fix: 根据屏幕刷新率设置等待时间,等待绘制结束后再调用draw方法获取方法

--- library/src/main/java/cn/chitanda/dynamicstatusbar/DynamicStatusBar.kt
+++ library/src/main/java/cn/chitanda/dynamicstatusbar/DynamicStatusBar.kt
@@ -4,14 +4,16 @@ import android.content.Context
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.Window
 import androidx.core.view.WindowInsetsControllerCompat
 import java.lang.ref.WeakReference
+import kotlin.math.roundToLong
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
 
 private const val TAG = "DynamicStatusBar"
 
@@ -22,7 +24,7 @@ object DynamicStatusBar {
     private var statusBarHeight: Int = 100
     private var weakDecorView: WeakReference<View>? = null
     private val decorView: View? get() = weakDecorView?.get()
-
+    private var delay = 1000L / 60
     internal fun init(context: Context) {
         getStatusBarHeight(context)
         initStatusBarBitmap()
@@ -31,23 +33,32 @@ object DynamicStatusBar {
     private val preDrawListener by lazy {
         ViewTreeObserver.OnPreDrawListener {
             if (initStatusBarBitmap()) {
-                statusBarCanvas?.let {
-                    decorView?.post {
-                        val backup = statusBarCanvas?.save()
-                        try {
-                            it.scale(1 / 5f, 1 / 5f)
-                            decorView?.draw(it)
-                        } catch (e: Exception) {
-                            Log.e(TAG, "OnPreDrawListener: ", e)
-                        } finally {
-                            backup?.let { statusBarCanvas?.restoreToCount(it) }
-                        }
-                        insetsController?.isAppearanceLightStatusBars =
-                            statusBarBitmap?.isLightColor() == true
-                    }
+                decorView?.handler?.let {
+                    it.removeCallbacksAndMessages(null)
+                    it.postDelayed(::calculateBright, delay)
                 }
             }
             true
+        }
+    }
+
+
+    private fun calculateBright() {
+        statusBarCanvas?.let {
+          val time =  measureTimeMillis{
+                val backup = statusBarCanvas?.save()
+                try {
+                    it.scale(1 / 5f, 1 / 5f)
+                    decorView?.draw(it)
+                } catch (e: Exception) {
+                    Log.e(TAG, "OnPreDrawListener: ", e)
+                } finally {
+                    backup?.let { statusBarCanvas?.restoreToCount(it) }
+                    insetsController?.isAppearanceLightStatusBars =
+                        statusBarBitmap?.isLightColor() == true
+                }
+            }
+            Log.d(TAG, "calculateBright: time = $time ms")
         }
     }
 
@@ -80,7 +91,12 @@ object DynamicStatusBar {
     }
 
     internal fun onResume(window: Window) {
-        weakDecorView = WeakReference(window.decorView)
+        weakDecorView = WeakReference(
+            if (window.decorView.fitsSystemWindows) window.decorView else window.decorView.findViewById(
+                android.R.id.content
+            )
+        )
+        delay =(1000L/ (window.decorView.display?.refreshRate?: 60f).roundToLong() )
         insetsController = WindowInsetsControllerCompat(window, window.decorView)
         decorView?.viewTreeObserver?.addOnPreDrawListener(preDrawListener)
     }
