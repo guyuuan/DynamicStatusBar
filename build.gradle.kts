cd5eca170e51443032c54476d2cfb8f3936e28a2
chunjinchen <chunjinchen@smartmiglobal.com>
2025-08-21 18:41:01 +0800

    feat: migrate publish script to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,20 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.nexus.publish)
+}
+val sonatypeUsername: String? by project
+val sonatypePassword: String? by project
+group = "io.github.guyuuan"
+nexusPublishing {
+    repositories {
+        // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
+        sonatype {
+            useStaging = true
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            username = sonatypeUsername ?: System.getenv("SONATYPE_USERNAME")
+            password = sonatypePassword ?: System.getenv("SONATYPE_PASSWORD")
+        }
+    }
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -5,6 +5,19 @@ plugins {
     alias(libs.plugins.signing)
 }
 
+fun getVersionNameFromGit(): String {
+    return try {
+        val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+        process.inputStream.bufferedReader().readText().trim()
+    } catch (e: Exception) {
+        "dev"
+    }
+}
+
+val isSnapshot =
+    (findProperty("isSnapshot")?.toString() ?: System.getenv("IS_SNAPSHOT"))?.toBoolean() ?: true
+version =
+    "${System.getenv(" VERSION_NAME ") ?: getVersionNameFromGit()}${if (isSnapshot) "-SNAPSHOT" else ""}"
 android {
     namespace = "io.github.guyuuan.dynamicstatusbar"
 
@@ -30,6 +43,9 @@ android {
     buildFeatures {
         buildConfig = true
     }
+    publishing {
+        singleVariant("release")
+    }
 }
 kotlin {
     jvmToolchain(21)
@@ -41,5 +57,54 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
 }
+val sonatypeUsername: String? by project // this is defined in ~/.gradle/gradle.properties
+val sonatypePassword: String? by project // this is defined in ~/.gradle/gradle.properties
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+            groupId = "io.github.guyuuan"
+            artifactId = "dynamicstatusbar"
+            logger.warn("version: ${project.version}")
+            this.version = project.version.toString()
 
-//apply from : '../publish.gradle'
+            pom {
+                name.set("DynamicStatusBar")
+                description.set("Automatically modify the color of the status bar icon.")
+                url.set("https://github.com/guyuuan/DynamicStatusBar")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("guyuuan")
+                        email.set("chunjinchen19998@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:guyuuan/DynamicStatusBar.git")
+                    developerConnection.set("scm:git:ssh://github.com/guyuuan/DynamicStatusBar.git")
+                    url.set("https://github.com/guyuuan/DynamicStatusBar")
+                }
+            }
+        }
+    }
+}
+val signingKeyId: String? =
+    System.getenv("SIGNING_KEY_ID") ?: project.findProperty("signing.keyId") as String?
+val signingPassword: String? =
+    System.getenv("SIGNING_PASSWORD") ?: project.findProperty("signing.password") as String?
+val signingSecretKey: String? =
+    System.getenv("SIGNING_SECRET_KEY") ?: project.findProperty("signing.secretKey") as String?
+signing {
+    useInMemoryPgpKeys(
+        signingKeyId, signingSecretKey, signingPassword
+    )
+    sign(publishing.publications["release"])
+}
